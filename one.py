e4b1423b16bea91424e08ca625cc9de0753e4fe5
ritishadhikari <ritishadhikari@gmail.com>
2024-05-24 10:08:14 +0530

    Till 05/24/2024 10:08:14

--- one.py
+++ one.py
@@ -1,11 +1,6 @@
 import asyncio
 
-# Define a Coroutine that simulates a time-consuming task
-async def fetch_data(delay):
-    print("Fetching Data ...")
-    await asyncio.sleep(delay=delay)
-    print("Data Fetched")
-    return {"data":"some data"}
+
 
 async def main():
     print("Start of main Coroutine") 
